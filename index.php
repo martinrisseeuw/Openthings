3c68da0dcf655a2a84f9f4c57b32d81a9396f613
Martin Risseeuw <hello@martinr.nl>
2014-12-08 12:37:49 +0100

    Update Martin

--- index.php
+++ index.php
@@ -45,62 +45,6 @@
 							</figure>
 						</article>
 					</li>
-					<li class="preview left">
-						<article>
-							<figure class="effect-lily">
-								<img src="public/images/shareable_energy.jpg" alt="img12"/>
-								<figcaption>
-									<div>
-										<h2 class="left">Shareable enegry</h2>
-										<p class="left">Selling excess solar or wind energy back to the power company is not a new idea. But what if...</p>
-									</div>
-									<a href="project.php">View more</a>
-								</figcaption>			
-							</figure>
-						</article>
-					</li>
-					<li class="preview left">
-						<article>
-							<figure class="effect-lily">
-								<img src="public/images/project_thumbnail.jpg" alt="img12"/>
-								<figcaption>
-									<div>
-										<h2 class="left">Shareable enegry</h2>
-										<p class="left">Selling excess solar or wind energy back to the power company is not a new idea. But what if...</p>
-									</div>
-									<a href="project.php">View more</a>
-								</figcaption>			
-							</figure>
-						</article>
-					</li>
-					<li class="preview left">
-						<article>
-							<figure class="effect-lily">
-								<img src="public/images/shareable_energy.jpg" alt="img12"/>
-								<figcaption>
-									<div>
-										<h2 class="left">Shareable enegry</h2>
-										<p class="left">Selling excess solar or wind energy back to the power company is not a new idea. But what if...</p>
-									</div>
-									<a href="project.php">View more</a>
-								</figcaption>			
-							</figure>
-						</article>
-					</li>
-					<li class="preview left">
-						<article>
-							<figure class="effect-lily">
-								<img src="public/images/project_thumbnail.jpg" alt="img12"/>
-								<figcaption>
-									<div>
-										<h2 class="left">Shareable enegry</h2>
-										<p class="left">Selling excess solar or wind energy back to the power company is not a new idea. But what if...</p>
-									</div>
-									<a href="project.php">View more</a>
-								</figcaption>			
-							</figure>
-						</article>
-					</li>
         </ul>
     </section>
     <?php include 'partials/footernav.html';?>
