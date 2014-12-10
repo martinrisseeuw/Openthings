ea8d364cef68e35f18aad3a1a4321f7e857a3a71
Martin Risseeuw <hello@martinr.nl>
2014-12-10 12:58:19 +0100

    update

---
+++ project.php
@@ -0,0 +1,9 @@
+<?php include 'partials/head.php';?>
+<section class="mainContent left">
+	<?php include 'modules/project-and-user-header.php';?>
+	<?php include 'modules/bom.php';?>
+	<?php include 'modules/project-details.php';?>
+	<?php include 'modules/comments-and-rating.php';?>
+	<?php include 'modules/project-versions.php';?>
+</section>
+<?php include 'partials/footer.php';?>
---
+++ user.php
@@ -0,0 +1,9 @@
+<?php include 'partials/head.php';?>
+    <section class="mainContent left">
+        <?php include 'modules/project-and-user-header.php';?>
+				<?php include 'modules/user-details.php';?>
+        <?php include 'modules/comments-and-rating.php';?>
+				<?php include 'modules/user-projects.php';?>
+			
+    </section>
+    <?php include 'partials/footer.php';?>
